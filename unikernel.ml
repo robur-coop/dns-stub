d6ee0f71c1675d090f79853a14f49a477167d891
Hannes Mehnert <hannes@mehnert.org>
2024-10-11 12:38:56 +0200

    adapt to mirage 4.8

--- unikernel.ml
+++ unikernel.ml
@@ -2,11 +2,11 @@ module K = struct
   open Cmdliner
   let dns_upstream =
     let doc = Arg.info ~doc:"Upstream DNS resolver" ["dns-upstream"] in
-    Arg.(value & (opt (some string) None doc))
+    Mirage_runtime.register_arg Arg.(value & (opt (some string) None doc))
 
   let dns_cache =
     let doc = Arg.info ~doc:"DNS cache size" ["dns-cache"] in
-    Arg.(value & (opt (some int) None doc))
+    Mirage_runtime.register_arg Arg.(value & (opt (some int) None doc))
 end
 
 open Lwt.Infix
@@ -17,9 +17,9 @@ module Main (R : Mirage_crypto_rng_mirage.S) (P : Mirage_clock.PCLOCK)
 
   module Stub = Dns_stub_mirage.Make(R)(Time)(P)(M)(S)
 
-  let start () () () () s dns_upstream cache_size =
+  let start () () () () s =
     let nameservers =
-      Option.map (fun ns -> [ ns ]) dns_upstream
+      Option.map (fun ns -> [ ns ]) (K.dns_upstream ())
     and primary_t =
       (* setup DNS server state: *)
       Dns_server.Primary.create ~rng:Mirage_crypto_rng.generate Dns_trie.empty
@@ -27,7 +27,7 @@ module Main (R : Mirage_crypto_rng_mirage.S) (P : Mirage_clock.PCLOCK)
     Stub.H.connect_device s >>= fun happy_eyeballs ->
     (* setup stub forwarding state and IP listeners: *)
     (try
-       Stub.create ?cache_size ?nameservers primary_t ~happy_eyeballs s
+       Stub.create ?cache_size:(K.dns_cache ()) ?nameservers primary_t ~happy_eyeballs s
      with
        Invalid_argument a ->
        Logs.err (fun m -> m "error %s" a);
